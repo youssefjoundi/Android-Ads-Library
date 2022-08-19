cb98b2ad6326c7eb89f271113ab560fad3780d75
youssefjoundi <yojoundi@student.1337.ma>
2022-08-19 21:17:29 +0100

    Fix ironsource bug1.0

--- library/src/main/java/com/solodroid/ads/sdk/format/InterstitialAd.java
+++ library/src/main/java/com/solodroid/ads/sdk/format/InterstitialAd.java
@@ -172,13 +172,6 @@ public class InterstitialAd {
             return this;
         }
 
-        private AdCloseListener interIron;
-
-        public Builder setInterIronsource(AdCloseListener interIron) {
-            this.interIron = interIron;
-            return this;
-        }
-
         public interface AdCloseListener {
             void onAdClosed();
         }
@@ -440,7 +433,7 @@ public class InterstitialAd {
                             @Override
                             public void onInterstitialAdClosed() {
                                 Log.d(TAG, "onInterstitialAdClosed");
-                                interIron.onAdClosed();
+                                adCloseListener.onAdClosed();
                                 loadInterstitialAd();
                             }
 
